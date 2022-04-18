f7a5a388671e01d2a56d03dfdabadaff059d7443
Tim Schneeberger <tim.schneeberger@outlook.de>
2022-04-18 23:23:42 +0200

    Add new client

---
+++ Ws2812AudioReactiveClient/Program.cs
@@ -0,0 +1,81 @@
+﻿using Ws2812LedController.AudioReactive.Effects.Fft;
+using Ws2812LedController.Core;
+using Ws2812LedController.Core.Model;
+using Ws2812LedController.UdpServer;
+using Ws2812LedController.UdpServer.Packets;
+
+namespace Ws2812AudioReactiveClient;
+
+public static class Entrypoint
+{
+    private const int FrameRate = 60;
+    
+    public static async Task Main()
+    {
+        var canvas = new RemoteLedCanvas(LayerId.ExclusiveEnetLayer, 0, /*124*/ 363, RenderMode.ManagedTask);
+
+        var mgr = new LedManager();
+        var remote = new LedStrip(new RemoteLedStrip(canvas));
+
+        var segmentBed = remote.CreateSegment(0, 124);
+        var segmentDeskL = remote.CreateSegment(124, 79);
+        var segmentDeskR = remote.CreateSegment(124+79, 79);
+        var segmentHeater = remote.CreateSegment(124+79*2, 81);
+        
+        mgr.RegisterSegment("full", remote.FullSegment);
+        mgr.RegisterSegment("bed", segmentBed);
+        mgr.RegisterSegment("desk_left", segmentDeskL);
+        mgr.RegisterSegment("desk_right", segmentDeskR);
+        mgr.RegisterSegment("heater", segmentHeater);
+
+
+        var client = new EnetClient("192.168.178.56");
+        canvas.NewPacketAvailable += (_, packet) => client.Queue.Enqueue(packet); 
+        client.Connect();
+        
+        await Task.Delay(100);
+
+        canvas.Bitmap.Clear();
+        canvas.Render();
+        
+        await mgr.Get("bed")!.SetEffectAsync(new FreqMapReactiveEffect()
+        {
+            Speed = 1000/FrameRate,
+           // FftCBinSelector = new(4),
+           // Threshold = 15
+            //Palette = new(Color.SlateBlue),
+            /*AvgSmoothingStrength = 5,
+            Intensity = 3,
+            EndFrequency = 120*/
+            //FftBinSelector = new(0)
+            //FadeStrength = 15,
+            //StartFromEdge = Edge.None
+        });
+        await mgr.Get("heater")!.SetEffectAsync(new RipplePeakReactiveEffect()
+        {
+            Speed = 1000/FrameRate,
+            FftCBinSelector = new(0),
+            Threshold = 15
+            
+            //FadeStrength = 15,
+            //StartFromEdge = Edge.None
+        });
+        /*await mgr.Get("heater")!.SetEffectAsync(new FreqMapReactiveEffect()
+        {
+            //FftBinSelector = new FftCBinSelector(0,3)
+            //Multiplier = 0.3
+            //FluentRainbow = true
+        });*/
+        
+        //mgr.MirrorTo("desk_left", "bed");
+        //mgr.MirrorTo("desk_left", "heater");
+        //mgr.MirrorTo("desk_left", "desk_right");
+        //mgr.Get("desk_left")!.SourceSegment.InvertX = true;
+        //mgr.Get("bed")!.SourceSegment.InvertX = true;
+
+        while (true)
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
